7b7ab9df9612c2ea692d305d00ad2f73bd4951c5
Oxylight <74827435+Oxylight@users.noreply.github.com>
2023-05-02 17:49:13 +0300

    Getopts bug fixes, some new logging.

--- README.md
+++ README.md
@@ -10,24 +10,25 @@ A script that notifies your audience that you have started a Twitch stream using
 ## Usage
 `./main.sh --discord_webhook <discord_webhook> --twitch_client_id <twitch_client_id> --twitch_client_secret <twitch_client_secret> --twitch_channel_login <twitch-channel-login> --alert_text <alert-text> --preview_url <preview_url> --channel_name <channel_name> --icon_url <icon_url> --color <color> --logs_dir <logs_dir> --work_dir <work_dir>`<br />
 ## Arguments
-| Name | Description | Required |
+| Name | Description | How to obtain / Default | Required |
 | --- | --- | --- |
-| `discord_webhook` | Discord webhook URL for sending alerts, you can create one at `<your server> -> Settings -> Integrations -> Webhooks` | Yes |
-| `twitch_client_id` | Twitch dev app client ID, (create app here: [Twitch Developers](https://dev.twitch.tv/console)) | Yes |
-| `twitch_client_secret` | Twitch dev app client secret, (create app here: [Twitch Developers](https://dev.twitch.tv/console)) | Yes |
-| `twitch_channel_login` | Twitch channel login that used to track streams (can be taken from URL https://twitch.tv/<twitch_login>) | Yes |
-| `alert_text` | Text that will be used for alerts. Default: `<channel_name> started a stream! @everyone`| No |
-| `preview_url` | URL that leads to image that will be used as preview. If not provided, Twitch preview will be used. | No |
-| `channel_name` | Custom channel name that will be used in alerts, does not affect twitch_channel_login. Default: Twitch username. | No |
-| `icon_url` | URL that leads to image that will be used as icon in alert. Default: avatar from Discord webhook. | No |
-| `color` | Color of the embed alert. Default: `6570404` (Hex -> Dec value) | No |
-| `logs_dir` | Directory where log will be stored. Default: `/var/log` | No |
-| `work_dir` | Directory where script will store data. Default: `/etc/twitch-discord-integration` | No |
+| `discord_webhook` | Discord webhook URL for sending alerts. | Create one at `<your server> -> Settings -> Integrations -> Webhooks`. | Yes |
+| `twitch_client_id` | Twitch dev app client ID. | Create app here: [Twitch Developers](https://dev.twitch.tv/console). | Yes |
+| `twitch_client_secret` | Twitch dev app client secret. | Create app here: [Twitch Developers](https://dev.twitch.tv/console). | Yes |
+| `twitch_channel_login` | Twitch channel login that used to track streams. | Can be taken from your Twitch URL https://twitch.tv/<twitch_login>. | Yes |
+| `alert_text` | Text that will be used for alerts. Can not begin with '--'. | Default: `<channel_name> started a stream! @everyone`. | No |
+| `preview_url` | URL that leads to image that will be used as preview. | If not provided, Twitch preview will be used. | No |
+| `channel_name` | Custom channel name that will be used in alerts, does not affect twitch_channel_login. Can not begin with '--'. | Default: Twitch username. | No |
+| `icon_url` | URL that leads to image that will be used as icon in alert. | Default: avatar from Discord webhook. | No |
+| `color` | Color of the embed alert. | Default: `6570404` (Hex -> Dec value). | No |
+| `logs_dir` | Directory where logs will be stored. | Default: `/var/log`. | No |
+| `work_dir` | Directory where script will store data. | Default: `/etc/twitch-discord-integration`. | No |
 ## Another information
 There's only one type of alert, you can create more using [python-discord-webhook docs](https://github.com/lovvskillz/python-discord-webhook/#basic-webhook) and inserting to current code [webhook.py](/webhook.py).<br />
 ## Disclaimer
 This project is not affiliated with Twitch or Discord or any other company. Any trademarks, logos, or brand names mentioned or shown in this project are the property of their respective owners. The use of such trademarks, logos, or brand names does not imply endorsement or affiliation with the project.
 ## TODO
-1. ~~Add to checking stream preview image saving system to be preview avaliable all the time~~ Done, need testing
-2. Create help page
-3. Create log-level setting
+1. ~~Add to checking stream preview image saving system to be preview avaliable all the time~~ Done, need testing.
+2. Create help page.
+3. Create log-level setting.
+4. Get user avatar from Twitch to use as icon_url.
--- main.sh
+++ main.sh
@@ -3,81 +3,120 @@
 #Getting arguments
 while getopts ":-:" optname; do
   case "$optname" in
-    "-")
+    -)
       case "${OPTARG}" in
-        discord_webhook) 
-          discord_webhook="$2"
+        discord_webhook)
+          discord_webhook="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$discord_webhook" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'discord_webhook' is required, exiting."
+            exit 1
+          fi
           ;;
         twitch_client_id)
-          twitch_client_id="$2"
+          twitch_client_id="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$twitch_client_id" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_client_id' is required, exiting."
+            exit 1
+          fi
           ;;
         twitch_client_secret)
-          twitch_client_secret="$2"
+          twitch_client_secret="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$twitch_client_secret" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_client_secret' is required, exiting."
+            exit 1
+          fi
           ;;
         twitch_channel_login)
-          twitch_channel_login="$2"
+          twitch_channel_login="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$twitch_channel_login" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_channel_login' is required, exiting."
+            exit 1
+          fi
           ;;
           alert_text)
-          alert_text="$2"
+          alert_text="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$alert_text" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'alert_text' is required, exiting."
+            exit 1
+          fi
           ;;
           preview_url)
-          preview_url="$2"
+          preview_url="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$preview_url" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'preview_url' is required, exiting."
+            exit 1
+          fi
           ;;
           channel_name)
-          channel_name="$2"
+          channel_name="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$channel_name" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'channel_name' is required, exiting."
+            exit 1
+          fi
           ;;
           icon_url)
-          icon_url="$2"
+          icon_url="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$icon_url" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'icon_url' is required, exiting."
+            exit 1
+          fi
           ;;
           color)
-          color="$2"
+          color="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$color" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'color' is required, exiting."
+            exit 1
+          fi
           ;;
           logs_dir)
-          logs="$2"
+          logs_dir="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$logs_dir" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'logs_dir' is required, exiting."
+            exit 1
+          fi
           ;;
           work_dir)
-          work_dir="$2"
+          work_dir="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          if [[ "$work_dir" == --* ]]; then
+            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'work_dir' is required, exiting."
+            exit 1
+          fi
           ;;
         *)
-          echo "Unknown option: --${OPTARG}"
+          echo "Unknown option: --${OPTARG}."
           exit 1
           ;;
       esac
       ;;
     "?")
-      echo "Unknown option: -$OPTARG"
-      exit 1
-      ;;
-    ":")
-      echo "No argument value for option: -$OPTARG"
+      echo "Unknown option: -$OPTARG."
       exit 1
       ;;
     *)
-      echo "Unknown error while processing options"
+      echo "Unknown error while processing options."
       exit 1
       ;;
   esac
 done
 
-#Checking required arguments
+#Checking required arguments (probalby legacy? need tests)
 if [[ -z "$discord_webhook" ]]; then
-  echo "Error: The 'discord_webhook' field is required."
+  echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'discord_webhook' is required, exiting."
   exit 1
 fi
 if [[ -z "$twitch_client_id" ]]; then
-  echo "Error: The 'twitch_client_id' field is required."
+  echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_client_id' is required, exiting."
   exit 1
 fi
 if [[ -z "$twitch_client_secret" ]]; then
-  echo "Error: The 'twitch_client_secret' field is required."
+  echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_client_secret' is required, exiting."
   exit 1
 fi
 if [[ -z "$twitch_channel_login" ]]; then
-  echo "Error: The 'twitch_channel_login' field is required."
+  echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_channel_login' is required, exiting."
   exit 1
 fi
 if [[ -z "$color" ]]; then
-  #(hex -> dec value)
   color=6570404
 fi
 if [[ -z "$logs_dir" ]]; then
@@ -85,7 +124,8 @@ if [[ -z "$logs_dir" ]]; then
   if [[ ! -d $logs_dir ]]; then
     mkdir -p $logs_dir
   fi
-  logs="$logs_dir/twitch-discord-integration-log.txt"
+  logs_file="$logs_dir/twitch-discord-integration-log.txt"
+  touch $logs_file
 fi
 if [[ -z "$work_dir" ]]; then
   work_dir='/etc/twitch-discord-integration'
@@ -100,12 +140,12 @@ if [ -e $work_dir/id.txt ]; then
 fi
 
 #Starting to work
-echo [$(date)] : Twitch-discord-integration : Started >> $logs
+echo "[$(date)] : Twitch-discord-integration : Started." >> $logs_file
 
 #Checking $discord_webhook for validity and setting up $icon_url
 discord_data=$(curl -s -X GET $discord_webhook)
 if [[ $(echo $discord_data | jq -r '.id') == null ]]; then
-  echo [$(date)] : ERROR : Twitch-discord-integration : Can not get discord webhook info, probably invalid webhook was provided, exiting >> $logs
+  echo "[$(date)] : ERROR : Twitch-discord-integration : Can not get discord webhook info. Probably invalid webhook have been provided, exiting." >> $logs_file
   exit 1
 fi
 if [[ -z "$icon_url" ]]; then
@@ -119,38 +159,41 @@ if [ -f $work_dir/twitch_token.txt ]; then
   twitch_token="${lines[0]}"
   expiration_time="${lines[1]}"
   if [[ $(date +%s) -ge $expiration_time ]]; then
-    response=$(curl --silent -X POST https://id.twitch.tv/oauth2/token -H 'Content-Type: application/x-www-form-urlencoded' -d "client_id=$twitch_client_id&client_secret=$twitch_client_secret&grant_type=client_credentials")
+    response=$(curl -s -X POST https://id.twitch.tv/oauth2/token -H 'Content-Type: application/x-www-form-urlencoded' \
+      -d "client_id=$twitch_client_id&client_secret=$twitch_client_secret&grant_type=client_credentials")
     oauth_token=$(echo $response | jq -r '.access_token')
     if [[ $oauth_token == null ]]; then
-      echo [$(date)] : ERROR : Twitch-discord-integration : Can not get oauth_token, exiting >> $logs
+      echo "[$(date)] : ERROR : Twitch-discord-integration : Can not get oauth_token, exiting." >> $logs_file
       exit 1
     fi
     expires_in_seconds=$(echo $response | jq -r '.expires_in')
     expiration_time=$(($(date +%s) + expires_in_seconds))
     echo $oauth_token > $work_dir/twitch_token.txt
     echo $expiration_time >> $work_dir/twitch_token.txt
-    echo [$(date)] : INFO : Twitch-discord-integration : Received new oauth_token >> $logs
+    echo "[$(date)] : INFO : Twitch-discord-integration : Received new oauth_token." >> $logs_file
   fi
-  echo [$(date)] : INFO : Twitch-discord-integration : Oauth_token not expired, using it >> $logs
+  echo "[$(date)] : INFO : Twitch-discord-integration : Oauth_token is not expired, using it." >> $logs_file
 else
-  response=$(curl --silent -X POST https://id.twitch.tv/oauth2/token -H 'Content-Type: application/x-www-form-urlencoded' -d "client_id=$twitch_client_id&client_secret=$twitch_client_secret&grant_type=client_credentials")
+  response=$(curl -s -X POST https://id.twitch.tv/oauth2/token -H 'Content-Type: application/x-www-form-urlencoded' \
+    -d "client_id=$twitch_client_id&client_secret=$twitch_client_secret&grant_type=client_credentials")
   oauth_token=$(echo $response | jq -r '.access_token')
   if [[ $oauth_token == null ]]; then
-    echo [$(date)] : ERROR : Twitch-discord-integration : Can not get oauth_token, exiting >> $logs
+    echo "[$(date)] : ERROR : Twitch-discord-integration : Can not get oauth_token, exiting." >> $logs_file
     exit 1
   fi
   expires_in_seconds=$(echo $response | jq -r '.expires_in')
   expiration_time=$(($(date +%s) + expires_in_seconds))
   echo $oauth_token > $work_dir/twitch_token.txt
   echo $expiration_time >> $work_dir/twitch_token.txt
-  echo [$(date)] : INFO : Twitch-discord-integration : Received new oauth_token >> $logs
+  echo "[$(date)] : INFO : Twitch-discord-integration : Received new oauth_token." >> $logs_file
 fi
 
 #Checking if stream is currently live
-chan_info=$(curl -s -X GET https://api.twitch.tv/helix/streams?user_login=$twitch_channel_login -H "Authorization: Bearer $oauth_token" -H "Client-Id: $twitch_client_id")
+chan_info=$(curl -s -X GET https://api.twitch.tv/helix/streams?user_login=$twitch_channel_login \
+  -H "Authorization: Bearer $oauth_token" -H "Client-Id: $twitch_client_id")
 is_live=$(echo $chan_info | jq -r '.data.type')
-if [[ $is_live == null || ! $is_live == "live" ]]; then
-  echo [$(date)] : OK : Twitch-discord-integration : Live stream is not detected, exiting >> $logs
+if [[ ! $is_live == "live" ]]; then
+  echo "[$(date)] : OK : Twitch-discord-integration : Live stream is not detected, exiting." >> $logs_file
   exit
 fi
 
@@ -168,7 +211,7 @@ fi
 #Checking that the stream ID has not changed since last launch (i.e., the stream is the same).
 if [[ $id == $(cat $work_dir/id.txt) ]]
 then
-  echo [$(date)] : OK : Twitch-discord-integration : The same stream detected, exiting >> $logs
+  echo "[$(date)] : OK : Twitch-discord-integration : The same stream detected, exiting." >> $logs_file
   exit
 fi
 echo $id > id.txt
@@ -176,14 +219,14 @@ echo $id > id.txt
 #if preview_url is not provided then use Twitch preview
 if [[ -z "$preview_url" ]]; then
   curl https://static-cdn.jtvnw.net/previews-ttv/live_user_$twitch_channel_login.jpg --silent -o $work_dir/preview.jpg
-  python3 webhook.py -webhook "$discord_webhook" -content "$alert_text" -stream_title "$title" -game "$game" -name "$channel_name" -url "https://www.twitch.tv/$twitch_channel_login" \
-  -icon_url "$icon_url" -color "$color" -preview "$work_dir/preview.jpg"  
+  python3 webhook.py -webhook "$discord_webhook" -content "$alert_text" -stream_title "$title" -game "$game" -name "$channel_name" \
+    -url "https://www.twitch.tv/$twitch_channel_login" -icon_url "$icon_url" -color "$color" -preview "$work_dir/preview.jpg"  
   rm $work_dir/preview.jpg
-  echo [$(date)] : OK : Twitch-discord-integration : Alert sent, using Twitch preview, exiting >> $logs
+  echo "[$(date)] : OK : Twitch-discord-integration : Alert sent with Twitch preview, exiting." >> $logs_file
   exit
 else
-  python3 webhook.py -webhook "$discord_webhook" -content "$alert_text" -stream_title "$title" -game "$game" -name "$channel_name" -url "https://www.twitch.tv/$twitch_channel_login" \
-  -icon_url "$icon_url" -color "$color" -preview_url "$preview_url"  
-  echo [$(date)] : OK : Twitch-discord-integration : Alert sent, using custom preview, exiting >> $logs
+  python3 webhook.py -webhook "$discord_webhook" -content "$alert_text" -stream_title "$title" -game "$game" -name "$channel_name" \
+    -url "https://www.twitch.tv/$twitch_channel_login" -icon_url "$icon_url" -color "$color" -preview_url "$preview_url"  
+  echo "[$(date)] : OK : Twitch-discord-integration : Alert sent with custom preview, exiting." >> $logs_file
   exit
 fi
