91173e911330138537ecee7ad9f2bf293d3386cb
Oxylight <74827435+Oxylight@users.noreply.github.com>
2023-07-04 23:06:11 +0300

    Added conf file, no arguments (only path to conf)

--- main.sh
+++ main.sh
@@ -1,159 +1,103 @@
 #!/bin/bash
 
-#Getting arguments
-while getopts ":-:" optname; do
-  case "$optname" in
-    -)
-      case "${OPTARG}" in
-        discord_webhook)
-          discord_webhook="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$discord_webhook" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'discord_webhook' is required, exiting."
-            exit 1
-          fi
-          ;;
-        twitch_client_id)
-          twitch_client_id="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$twitch_client_id" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_client_id' is required, exiting."
-            exit 1
-          fi
-          ;;
-        twitch_client_secret)
-          twitch_client_secret="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$twitch_client_secret" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_client_secret' is required, exiting."
-            exit 1
-          fi
-          ;;
-        twitch_channel_login)
-          twitch_channel_login="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$twitch_channel_login" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_channel_login' is required, exiting."
-            exit 1
-          fi
-          ;;
-          alert_text)
-          alert_text="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$alert_text" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'alert_text' is required, exiting."
-            exit 1
-          fi
-          ;;
-          preview_url)
-          preview_url="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$preview_url" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'preview_url' is required, exiting."
-            exit 1
-          fi
-          ;;
-          channel_name)
-          channel_name="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$channel_name" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'channel_name' is required, exiting."
-            exit 1
-          fi
-          ;;
-          icon_url)
-          icon_url="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$icon_url" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'icon_url' is required, exiting."
-            exit 1
-          fi
-          ;;
-          color)
-          color="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$color" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'color' is required, exiting."
-            exit 1
-          fi
-          ;;
-          logs_dir)
-          logs_dir="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$logs_dir" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'logs_dir' is required, exiting."
-            exit 1
-          fi
-          ;;
-          work_dir)
-          work_dir="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-          if [[ "$work_dir" == --* ]]; then
-            echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'work_dir' is required, exiting."
-            exit 1
-          fi
-          ;;
-        *)
-          echo "Unknown option: --${OPTARG}."
-          exit 1
-          ;;
-      esac
+# Reading config - default or specified
+while getopts ":c:" opt; do
+  case $opt in
+    c)
+      config=$OPTARG
       ;;
-    "?")
-      echo "Unknown option: -$OPTARG."
-      exit 1
-      ;;
-    *)
-      echo "Unknown error while processing options."
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
       exit 1
       ;;
   esac
 done
+if [[ $config ]]; then
+  source $config
+else
+  source ./tw-dis.conf
+fi
 
-#Checking required arguments (probalby legacy? need tests)
-if [[ -z "$discord_webhook" ]]; then
-  echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'discord_webhook' is required, exiting."
-  exit 1
-fi
-if [[ -z "$twitch_client_id" ]]; then
-  echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_client_id' is required, exiting."
-  exit 1
-fi
-if [[ -z "$twitch_client_secret" ]]; then
-  echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_client_secret' is required, exiting."
-  exit 1
-fi
-if [[ -z "$twitch_channel_login" ]]; then
-  echo "[$(date)] : ERROR : Twitch-discord-integration pre-check : Argument 'twitch_channel_login' is required, exiting."
-  exit 1
-fi
-if [[ -z "$color" ]]; then
-  color=6570404
-fi
+# Configuring environment for logs
 if [[ -z "$logs_dir" ]]; then
-  logs_dir='/var/log'
+  logs_dir='/var/log/tw-dis'
 fi
 if [[ ! -d $logs_dir ]]; then
   mkdir -p $logs_dir
 fi
-logs_file="$logs_dir/twitch-discord-integration-log.txt"
+logs_file="$logs_dir/tw-dis.log"
 touch $logs_file
+# Check if logs file size is greater than 10MB, if yes then move to logs_file.1
+if [[ $(stat -c%s "$logs_file") -gt 10485760 ]]; then
+  i=1
+  last_logs_file="$logs_file.$i"
+  while [[ -e "$last_logs_file" ]]; do
+    ((i++))
+    last_logs_file="$logs_file.$i"
+  done
+  # Rename existing logs_files
+  for ((j=i-1; j>=1; j--)); do
+    existing_logs_file="$logs_file.$j"
+    new_existing_logs_file="$logs_file.$((j+1))"
+    if [[ -e "$existing_logs_file" ]]; then
+      mv "$existing_logs_file" "$new_existing_logs_file"
+    fi
+  done
+  mv "$logs_file" "$logs_file.1"
+fi
+
+# Configuring environment for work dir
 if [[ -z "$work_dir" ]]; then
-  work_dir='/etc/twitch-discord-integration'
+  work_dir='/etc/tw-dis'
 fi
 if [[ ! -d $work_dir ]]; then
   mkdir -p $work_dir
 fi
 
-#Checking stream ID file
+# Checking required variables
+if [[ -z "$discord_webhook" ]]; then
+  echo "[$(date)] : ERROR : Twitch-discord-integration check : Variable 'discord_webhook' is required, exiting."
+  exit 1
+fi
+if [[ -z "$twitch_client_id" ]]; then
+  echo "[$(date)] : ERROR : Twitch-discord-integration check : Variable 'twitch_client_id' is required, exiting."
+  exit 1
+fi
+if [[ -z "$twitch_client_secret" ]]; then
+  echo "[$(date)] : ERROR : Twitch-discord-integration check : Variable 'twitch_client_secret' is required, exiting."
+  exit 1
+fi
+if [[ -z "$twitch_channel_login" ]]; then
+  echo "[$(date)] : ERROR : Twitch-discord-integration check : Variable 'twitch_channel_login' is required, exiting."
+  exit 1
+fi
+if [[ -z "$color" ]]; then
+  color=6570404
+fi
+
+# Starting to work
+echo "[$(date)] : INFO : Twitch-discord-integration : Started." >> $logs_file
+
+# Checking stream ID file
 if [ ! -e $work_dir/id.txt ]; then
   echo -1 > $work_dir/id.txt
 fi
 
-#Starting to work
-echo "[$(date)] : INFO : Twitch-discord-integration : Started." >> $logs_file
-
-#Checking $discord_webhook for validity and setting up $icon_url
+# Checking $discord_webhook for validity
 discord_data=$(curl -s -X GET $discord_webhook)
 if [[ $(echo $discord_data | jq -r '.id') == null ]]; then
   echo "[$(date)] : ERROR : Twitch-discord-integration : Can not get discord webhook info. Probably invalid webhook have been provided, exiting." >> $logs_file
   exit 1
 fi
+
+# Setting up $icon_url
 if [[ -z "$icon_url" ]]; then
-  #if icon_url is not provided, then Discord webhook avatar is used
+  # If icon_url is not provided, then Discord webhook avatar is used
   icon_url="https://cdn.discordapp.com/avatars/$(echo $discord_data | jq -r '.id')/$(echo $discord_data | jq -r '.avatar').png"
+  # TODO: use Twitch channel icon instead of Discord webhook icon or let user choose in conf
 fi
 
-#Receiving $oauth_token for Twtich
+# Receiving $oauth_token for Twtich
 response=$(curl -s -X POST https://id.twitch.tv/oauth2/token -H 'Content-Type: application/x-www-form-urlencoded' \
   -d "client_id=$twitch_client_id&client_secret=$twitch_client_secret&grant_type=client_credentials")
 oauth_token=$(echo $response | jq -r '.access_token')
@@ -163,7 +107,7 @@ if [[ $oauth_token == null ]]; then
 fi
 echo "[$(date)] : INFO : Twitch-discord-integration : Received new oauth_token." >> $logs_file
 
-#Checking if stream is currently live
+# Checking if stream is currently live
 chan_info=$(curl -s -X GET https://api.twitch.tv/helix/streams?user_login=$twitch_channel_login \
   -H "Authorization: Bearer $oauth_token" -H "Client-Id: $twitch_client_id")
 is_live=$(echo $chan_info | jq -r '.data[0].type')
@@ -172,7 +116,7 @@ if [[ ! $is_live == "live" ]]; then
   exit
 fi
 
-#Getting stream data
+# Getting stream data
 title=$(echo $chan_info | jq -r '.data[0].title')
 game=$(echo $chan_info | jq -r '.data[0].game_name')
 id=$(echo $chan_info | jq -r '.data[0].id')
@@ -183,7 +127,7 @@ if [[ -z "$alert_text" ]]; then
   alert_text="$channel_name started a stream! @everyone"
 fi
 
-#Checking that the stream ID has not changed since last launch (i.e., the stream is the same).
+# Checking that the stream ID has not changed since last script launch (i.e., the stream is the same).
 if [[ $id == $(cat $work_dir/id.txt) ]]
 then
   echo "[$(date)] : OK : Twitch-discord-integration : The same stream detected, exiting." >> $logs_file
@@ -191,18 +135,18 @@ then
 fi
 echo $id > $work_dir/id.txt
 
-#if preview_url is not provided then use Twitch preview
+# If preview_url is not provided then use Twitch preview
 if [[ -z "$preview_url" ]]; then
   curl https://static-cdn.jtvnw.net/previews-ttv/live_user_$twitch_channel_login.jpg --silent -o $work_dir/preview.jpg
   python3 "$(dirname "$(realpath "$0")")"/webhook.py -webhook "$discord_webhook" -content "$alert_text" -stream_title "$title" -game "$game" -name "$channel_name" \
     -url "https://www.twitch.tv/$twitch_channel_login" -icon_url "$icon_url" -color "$color" -preview "$work_dir/preview.jpg"  
-  rm $work_dir/preview.jpg
   if [ $? -eq 0 ]
   then
-      echo "Python script executed successfully."
+      echo "Python script executed successfully." #DEBUG
   else
-      echo "Python script encountered an error."
+      echo "Python script encountered an error." #DEBUG
   fi
+  rm $work_dir/preview.jpg
   echo "[$(date)] : OK : Twitch-discord-integration : Alert sent with Twitch preview, exiting." >> $logs_file
   exit
 else
